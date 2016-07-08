f95e54ab38f7561a48511bd350a4b3338ff6143b
Tina Pastelero <tinapastelero@gmail.com>
2016-07-08 02:50:50 +0000

    fixed broken accounting file

--- accounting.py
+++ accounting.py
@@ -1,55 +1,82 @@
-melon_cost = 1.00
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+def customer_check(file_path):  
+    """Checks if customers in file have over or underpaid
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+    Open source file, read each line of code, write a new file with results of whether customers have over or underpaid """
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+    customer_log = open(file_path)
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
+    for line in customer_log:
+        line = line.rstrip()
+        customer_output = line.split('|')
+        customer_number = customer_output[0]
+        customer_name = customer_output[1]
+        expected_payment = float(customer_output[2])
+        actual_payment = float(customer_output[3])
 
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
+        if actual_payment > expected_payment:
+            overpayment = actual_payment - expected_payment
+            print "{} {} overpaid by ${:.2f}".format(customer_number, customer_name,overpayment)
+        elif actual_payment < expected_payment:
+            underpayment = expected_payment - actual_payment
+            print "{} {} underpaid by ${:.2f}".format(customer_number, customer_name,underpayment)
 
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
+    customer_log.close()
 
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print customer1_name, "paid {:.2f}, expected {:.2f}".format(
-        customer1_paid, customer1_expected)
+customer_check("customer-orders.txt")
 
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print customer2_name, "paid {:.2f}, expected {:.2f}".format(
-        customer2_paid, customer2_expected)
+# melon_cost = 1.00
 
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print customer3_name, "paid {:.2f}, expected {:.2f}".format(
-        customer3_paid, customer3_expected)
+# customer1_name = "Joe"
+# customer1_melons = 5
+# customer1_paid = 5.00
 
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print customer4_name, "paid {:.2f}, expected {:.2f}".format(
-        customer4_paid, customer4_expected)
+# customer2_name = "Frank"
+# customer2_melons = 6
+# customer2_paid = 6.00
 
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print customer5_name, "paid {:.2f}, expected {:.2f}".format(
-        customer5_paid, customer5_expected)
+# customer3_name = "Sally"
+# customer3_melons = 3
+# customer3_paid = 3.00
 
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print customer6_name, "paid {:.2f}, expected {:.2f}".format(
-        customer6_paid, customer6_expected)
+# customer4_name = "Sean"
+# customer4_melons = 9
+# customer4_paid = 9.50
+
+# customer5_name = "David"
+# customer5_melons = 4
+# customer5_paid = 4.00
+
+# customer6_name = "Ashley"
+# customer6_melons = 3
+# customer6_paid = 2.00
+
+# customer1_expected = customer1_melons * melon_cost
+# if customer1_expected != customer1_paid:
+#     print customer1_name, "paid {:.2f}, expected {:.2f}".format(
+#         customer1_paid, customer1_expected)
+
+# customer2_expected = customer2_melons * melon_cost
+# if customer2_expected != customer2_paid:
+#     print customer2_name, "paid {:.2f}, expected {:.2f}".format(
+#         customer2_paid, customer2_expected)
+
+# customer3_expected = customer3_melons * melon_cost
+# if customer3_expected != customer3_paid:
+#     print customer3_name, "paid {:.2f}, expected {:.2f}".format(
+#         customer3_paid, customer3_expected)
+
+# customer4_expected = customer4_melons * melon_cost
+# if customer4_expected != customer4_paid:
+#     print customer4_name, "paid {:.2f}, expected {:.2f}".format(
+#         customer4_paid, customer4_expected)
+
+# customer5_expected = customer5_melons * melon_cost
+# if customer5_expected != customer5_paid:
+#     print customer5_name, "paid {:.2f}, expected {:.2f}".format(
+#         customer5_paid, customer5_expected)
+
+# customer6_expected = customer6_melons * melon_cost
+# if customer6_expected != customer6_paid:
+#     print customer6_name, "paid {:.2f}, expected {:.2f}".format(
+#         customer6_paid, customer6_expected)
